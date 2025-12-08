fb3869242f19236058392f64240501b0da3ac380
Gabriel Athia <g.athia@nsaglobal.net>
2025-12-08 12:45:58 -0300

    Initial Commit

---
+++ src/test/java/com/docescarol/pricing_system/PricingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docescarol.pricing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
